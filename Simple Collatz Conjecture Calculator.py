e4044075a1dc7b443b560d21d6e1918a6b2310cf
Anonymous <80660607+numpy-ai@users.noreply.github.com>
2022-04-28 01:10:36 +0900

    예외처리를 추가하여 정수 외의 것이 입력되면 걸러지게 함. 콜라츠 추측에 숫자를 입력해달라는 문장 추가. 여러 변수 명도 함께 변경.

--- Simple Collatz Conjecture Calculator.py
+++ Simple Collatz Conjecture Calculator.py
@@ -1,44 +1,47 @@
-
 # 콜라츠 추측의 계산에 따라 값을 구하는 함수
 # @param 2 이상의 정수
-def col(x) :
-	while True :
-		if x % 2 == 0 :
-			x /= 2
-			print(x)
-			if x == 1:
-				break
-		if x % 2 == 1 :
-			x  = 3 * x + 1
-			print(x)
-			if x == 1:
-				break
+def toCol(num):
+    while True:
+        if num % 2 == 0:
+            num /= 2
+            print(num)
+            if num == 1:
+                break
+        if num % 2 == 1:
+            num = 3 * num + 1
+            print(num)
+            if num == 1:
+                break
 
-x_list = []
-while True :
-    print(" 1. 콜라츠 추측 계산하기\n 2. 지금까지 입력한 숫자 확인하기\n 3. 숫자 기록 삭제하기\n 4. 종료하기")
-    select = int(input())
-    
-    if select == 1 :
-        x = int(input())
-        col(x)
-        x_list.append(x)
-    
-    elif select == 2 :
-        cnt = 1
-        for i in range(len(x_list)) :
-            print("#%d: %d " %(cnt, x_list[i]))
-            cnt += 1
-        if not x_list :
-            print("기록된 숫자가 없습니다.")
+logList = []
 
-    elif select == 3 :
-        print("삭제했습니다.")
-        x_list.clear()
-        
-    elif select == 4 :
-        print("종료합니다.")
-        break
-    
-    else :
-        print("1~4번 기능만 구현돼있습니다.")
+while True:
+    try:
+        print(" 1. 콜라츠 추측 계산하기\n 2. 지금까지 입력한 숫자 확인하기\n 3. 숫자 기록 삭제하기\n 4. 종료하기")
+        select = int(input())
+
+        if select == 1:
+            isInputNum = int(input("숫자를 입력해주세요: "))
+            toCol(isInputNum)
+            logList.append(isInputNum)
+
+        elif select == 2:
+            cnt = 1
+            for i in range(len(logList)):
+                print("#%d: %d " % (cnt, logList[i]))
+                cnt += 1
+            if not logList:
+                print("기록된 숫자가 없습니다.")
+
+        elif select == 3:
+            print("삭제했습니다.")
+            logList.clear()
+
+        elif select == 4:
+            print("종료합니다.")
+            break
+
+        else:
+            print("1~4번 기능만 구현돼 있습니다.")
+    except ValueError:
+        print("1~4번만 입력해주시길 바랍니다.")
